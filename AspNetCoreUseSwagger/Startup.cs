340cdfb0dc0e8f1b0cc20f9a1f3bd98b37dbccdc
毕殿卿 <bdq123@outlook.com>
2019-01-10 21:03:04 +0800

    *

--- AspNetCoreUseSwagger/Startup.cs
+++ AspNetCoreUseSwagger/Startup.cs
@@ -25,7 +25,7 @@ namespace AspNetCoreUseSwagger
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             // Register the Swagger generator, defining one or more Swagger documents
             // https://docs.microsoft.com/zh-cn/aspnet/core/tutorials/getting-started-with-swashbuckle
