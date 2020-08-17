30b4ed80a0729e1a2105d8c250364613c3fe5582
毕殿卿 <dianqing.bi@drcuiyutao.com>
2020-08-17 16:08:01 +0800

    3.1

--- AspNetCoreUseSwagger/Startup.cs
+++ AspNetCoreUseSwagger/Startup.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Collections.Generic;
@@ -25,31 +25,29 @@ namespace AspNetCoreUseSwagger
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
 
             // Register the Swagger generator, defining one or more Swagger documents
             // https://docs.microsoft.com/zh-cn/aspnet/core/tutorials/getting-started-with-swashbuckle
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+                //c.EnableAnnotations();
 
                 c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "AspNetCoreUseSwagger.xml"), true);
                 c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Domain.xml"), true);
 
-                c.OperationFilter<AddAuthTokenOperationFilter>();
+                //c.OperationFilter<AddAuthTokenOperationFilter>();
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
             }
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
@@ -61,31 +59,35 @@ namespace AspNetCoreUseSwagger
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
 
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 
 
     public class AddAuthTokenOperationFilter : IOperationFilter
     {
-        public void Apply(Operation operation, OperationFilterContext context)
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
         {
             if (operation.Parameters == null)
             {
-                operation.Parameters = new List<IParameter>();
+                operation.Parameters = new List<OpenApiParameter>();
             }
 
             context.ApiDescription.TryGetMethodInfo(out MethodInfo methodInfo);
             var allowAnonymousAttribute = methodInfo.GetCustomAttribute<AllowAnonymousAttribute>(false);
+
             if (allowAnonymousAttribute == null)
             {
-                operation.Parameters.Add(new NonBodyParameter()
+                operation.Parameters.Add(new OpenApiParameter()
                 {
                     Name = "Authorization",
-                    In = "header",
-                    Type = "string",
+                    In = ParameterLocation.Header,
                     Description = "Authorization认证信息",
-                    Default = "Bearer ",
                     Required = true
                 });
             }
