585dcb4b7a43192c9984f92668dc0e65febcc0eb
bidianqing <bidianqing@qq.com>
2022-07-23 01:34:09 +0800

    *

--- AspNetCoreUseSwagger/Startup.cs
+++ AspNetCoreUseSwagger/Startup.cs
@@ -114,10 +114,7 @@ namespace AspNetCoreUseSwagger
                 app.UseSwagger();
 
                 // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                });
+                app.UseSwaggerUI();
             }
 
 
